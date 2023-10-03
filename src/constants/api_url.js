a84b4a10147bdfe7d8fd36cd35b45d4150339e8c
Joshua Permito <dwigoric@insanedevs.com>
2023-10-03 18:54:34 +0800

    refactor: use Vuetify components + integrate JS

---
+++ src/constants/api_url.js
@@ -0,0 +1,2 @@
+// Change this to deployed URL when deploying
+export const API_URL = 'http://localhost:3000'
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,32 +1,66 @@
+<script setup>
+// Import vue components
+import { ref } from 'vue'
+import router from '../router'
+
+// Import constants
+import { API_URL } from '../constants'
+
+// Define refs
+const username = ref('')
+const password = ref('')
+const form = ref(null)
+const errorMessage = ref('')
+
+// Define methods
+const logIn = async () => {
+    const formData = new FormData(form.value)
+
+    // TODO: Finalize login endpoint
+    const response = await fetch(`${API_URL}/login`, {
+        method: 'POST',
+        body: formData
+    })
+
+    // TODO: Finalize response format
+    const data = await response.json()
+    if (data.success) {
+        await router.push('/dashboard')
+    } else {
+        errorMessage.value = data.message
+    }
+}
+</script>
+
 <!-- TODO: Create Login Page -->
 
 <template>
-<main>
     <div class="wrapper">
-            <div class="login">
-                <div class="header">Log In</div>
-                <form id="login-form">
-                    <div class="username-pw-input">
-                        <input type="text" id="login-username" required>
-                        <label>Username</label>
-                    </div>
+        <div class="login">
+            <div class="header">Log In</div>
+            <VForm id="login-form" ref="form">
+                <VTextField
+                    v-model="username"
+                    id="login-username"
+                    class="username-pw-input"
+                    label="Username"
+                    required
+                />
+                <VTextField
+                    v-model="password"
+                    id="login-pw"
+                    class="username-pw-input"
+                    label="Password"
+                    required
+                />
 
-                    <div class="username-pw-input">
-                        <input type="password" id="login-pw" required>
-                        <label>Password</label>
-                    </div>
+                <!--<div class="rememberMe">-->
+                <!--    <label><input type="checkbox" id="login-rememberMe" />Remember Me </label>-->
+                <!--</div>-->
 
-                    <!-- <div class="rememberMe">
-                        <label><input type="checkbox" id="login-rememberMe">Remember Me </label>
-                    </div> -->
-                    
-                    <button type="submit" class="btn" onclick="logIn">Log In</button>
-                    <div class="error" id="login-error"> </div>
-                </form>
-            </div>
+                <VBtn type="submit" class="btn" @click.prevent="logIn">Log In</VBtn>
+                <div v-if="errorMessage" class="error" id="login-error">{{ errorMessage }}</div>
+            </VForm>
         </div>
-</main>
+    </div>
 </template>
-
-<!-- <script src="/src/js/login.js"></script> -->
-
