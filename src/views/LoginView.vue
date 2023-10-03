4d21b269d3f6d162e5973b02b50aeee2915f2d07
anaj00 <bantolino.jana@gmail.com>
2023-10-03 16:34:20 +0800

    Created Frontend of Login Page

---
+++ src/views/LoginView.vue
@@ -0,0 +1,32 @@
+<!-- TODO: Create Login Page -->
+
+<template>
+<main>
+    <div class="wrapper">
+            <div class="login">
+                <div class="header">Log In</div>
+                <form id="login-form">
+                    <div class="username-pw-input">
+                        <input type="text" id="login-username" required>
+                        <label>Username</label>
+                    </div>
+
+                    <div class="username-pw-input">
+                        <input type="password" id="login-pw" required>
+                        <label>Password</label>
+                    </div>
+
+                    <!-- <div class="rememberMe">
+                        <label><input type="checkbox" id="login-rememberMe">Remember Me </label>
+                    </div> -->
+                    
+                    <button type="submit" class="btn" onclick="logIn">Log In</button>
+                    <div class="error" id="login-error"> </div>
+                </form>
+            </div>
+        </div>
+</main>
+</template>
+
+<!-- <script src="/src/js/login.js"></script> -->
+
