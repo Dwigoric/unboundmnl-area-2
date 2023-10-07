90ed31d0d1e347ecb6e5994a2dc13494b5935f07
anaj00 <bantolino.jana@gmail.com>
2023-10-07 21:22:04 +0800

    feat: Added fields for user profile creation

--- src/components/UserProfileRegister.vue
+++ src/components/UserProfileRegister.vue
@@ -19,39 +19,54 @@
 }
 
 .wrapper {
-    position: sticky;
-
     background: var(--vt-c-white);
     border-radius: 5px;
 
-    width: 30%;
-    padding: 3%;
-    padding-top: 2%;
+    width: 80vw;
+    max-height: 80vh;
 
-    /* display: flex;
-    justify-content: center; */
+    display: flex;
+    flex-direction: column;
     overflow: auto;
 }
 
-.login {
-    /* border: 1px solid black; */
+
+.formDiv {
 }
 
 .header {
+    /* border: 1px solid black; */
+    background-color: var(--vt-c-white-off);
+    width: 100%;
+    padding: 2%;
+    
+    position: sticky;
+    top: 0;
+    z-index: 1000;
+
     font-size: 1.5rem;
     font-weight: bold;
 
-    color: var(--vt-c-black);
     text-align: center;
     margin-bottom: 3%;
+}
+
+.header2 {
+    font-size: 1.2rem;
+    margin-bottom: 3%;
+    font-weight: bold;
+}
+
+.infoFields {
+    padding: 3%;
+
 }
 
 
 .username-pw-input {}
 
-.btn {
-    width: 100%;
 
+.btn {
     color: var(--vt-c-white-off);
     font-weight: 600;
     background: var(--vt-c-blue);
@@ -59,6 +74,12 @@
     border: none;
     border-radius: 5px;
     cursor: pointer;
+}
+
+.btnWrapper {
+    /* border: 1px solid black; */
+    display: flex;
+    justify-content: flex-end;
 }
 
 .capitalize-text {
@@ -95,23 +116,37 @@
     <div class="bg">
         <div class="wrapper">
             <div class="header">Create a User Profile</div>
-            <div class="login">
-                <VForm id="login-form" ref="form">
-                    <!-- TODO: [User] Create proper fields -->
-                    <VTextField class="username-pw-input" v-model="first_name" id="login-pw" label="Last Name" required />
-                    <VTextField class="username-pw-input" v-model="last_name" id="login-pw" label="First Name" required />
-                    <VTextField class="username-pw-input" v-model="username" id="login-username" label="Age" required />
-                    <VTextField class="username-pw-input" v-model="password" id="login-pw" label="City" required />
+            <div class="infoFields">
+                <div class="header2">Borrower's Information</div>
+                <div class="formDiv">
+                    <VForm id="login-form" ref="form">
+                        <!-- TODO: [User] Create proper fields -->
+                        <VTextField class="username-pw-input" v-model="first_name" id="login-pw" label="Last Name" required />
+                        <VTextField class="username-pw-input" v-model="last_name" id="login-pw" label="First Name" required />
+                        <VTextField class="username-pw-input" v-model="last_name" id="login-pw" label="Middle Name" required />
+                        <VTextField class="username-pw-input" v-model="username" id="login-username" label="Date of Birth" required />
+                        <VTextField class="username-pw-input" v-model="password" id="login-pw" label="Age" required />
+                        <VTextField class="username-pw-input" v-model="last_name" id="login-pw" label="Gender" required />
+                        <VTextField class="username-pw-input" v-model="last_name" id="login-pw" label="Tin Number" required />
+                        <VTextField class="username-pw-input" v-model="last_name" id="login-pw" label="Civil Status" required />
+                        <VTextField class="username-pw-input" v-model="last_name" id="login-pw" label="Contact Number" required />
+                        <VTextField class="username-pw-input" v-model="last_name" id="login-pw" label="Residence Address" required />
+                        <VTextField class="username-pw-input" v-model="last_name" id="login-pw" label="Monthly Income" required />
+                        <VTextField class="username-pw-input" v-model="last_name" id="login-pw" label="Occupation/Source of Income" required />
 
 
-                    <!-- <div class="rememberMe">
-                    <label><input type="checkbox" id="login-rememberMe" />Remember Me </label>
-                    </div> -->
 
-                    <VBtn type="submit" class="btn capitalize-text" @click.prevent="logIn">Register Officer</VBtn>
-                    <div v-if="errorMessage" class="error" id="login-error"></div>
-                </VForm>
-            </div>
+                        <!-- <div class="rememberMe">
+                        <label><input type="checkbox" id="login-rememberMe" />Remember Me </label>
+                        </div> -->
+
+                        <div class="btnWrapper">
+                            <VBtn type="submit" class="btn capitalize-text" @click.prevent="logIn">Create User Profile</VBtn>
+                        </div>
+                        <div v-if="errorMessage" class="error" id="login-error"></div>
+                    </VForm>
+                    </div>
+                </div>
         </div>
     </div>
 </template>
